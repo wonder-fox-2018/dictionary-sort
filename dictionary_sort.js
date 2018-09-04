e6f312e51f4ff8e7ee6fea7bc2c9d4d9ab373e67
zenfinish <zen.finish@gmail.com>
2018-09-04 22:52:28 +0700

    Upload

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,27 @@
 const readline = require('readline');
-// your code here to initialize the program and take user input
+function Dictionary(data){
+    for (var i = 0; i < data.length - 1; i++){
+        
+        var dataSekarang = data[i];
+        var nilaiMin = data[i+1];
+        var indexNilaiMin = i+1;
+        for (var j = i + 1; j < data.length; j++){
+            if (data[j] < nilaiMin){
+                nilaiMin = data[j];
+                indexNilaiMin = j;
+            }
+        }
+
+        if (nilaiMin < dataSekarang){
+            data[i] = nilaiMin;
+            data[indexNilaiMin] = dataSekarang;
+        }
+        
+    }
+
+    return data.toString();
+}
+console.log(Dictionary(['makan','duduk','tidur','terbang'])) // duduk, makan, tidur, terbang
+console.log(Dictionary(['anggi','angga','ani','adi'])) // angga, anggi, ani, adi
 
 module.exports = Dictionary
