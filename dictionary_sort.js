dd8f4ed39d950febe00cf75ed2c2f5127e5dc1ad
fathulqorib <adie.share@gmail.com>
2018-09-04 16:53:42 +0700

    make sense

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -3,8 +3,8 @@ const readline = require('readline');
 function DictionarySort(arr){
     for (let i = 0; i < arr.length; i++) {
         let tmp = arr[i]
-        for (let j = 0; j < arr.length; j++) {
-            if(arr[j]>=tmp){
+        for (let j = i+1; j < arr.length; j++) {
+            if(arr[j]<=tmp){
                 tmp = arr[j]
                 arr[j] = arr[i]
                 arr[i] = tmp
