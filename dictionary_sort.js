07f446246b16fbd51788fb827060e61f32a6f8fe
hacktiv8 <hacktiv8@H8JKT009.localdomain>
2018-09-04 11:19:46 +0700

    ganti sorting algo ke bubble sort

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,18 +1,34 @@
 const readline = require('readline');
 // your code here to initialize the program and take user input
 function dictSort(arr) {
-  let unsorted,j;
+  // let unsorted,j;
 
-  for (let i = 1; i <= arr.length; i++) {
-    unsorted = arr[i]
-    j = i-1
+  // for (let i = 1; i <= arr.length; i++) {
+  //   unsorted = arr[i]
+  //   j = i-1
 
-    while (j>=0 && unsorted < arr[j]) {
-      arr[j+1] = arr[j]
-      j -= 1
-      arr[j+1] = unsorted
+  //   while (j>=0 && unsorted < arr[j]) {
+  //     arr[j+1] = arr[j]
+  //     j -= 1
+  //     arr[j+1] = unsorted
+  //   }
+  // }
+  // return arr
+
+  /*----------- Bubble Sort --------------*/
+  let temp;
+  do {
+    swapped = false
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i-1] > arr[i]) {
+        temp = arr[i-1]
+        arr[i-1] = arr[i]
+        arr[i] = temp
+        swapped = true
+      }
     }
-  }
+  } while (swapped);
+
   return arr
 }
 let arrOfWord1 = ['makan','duduk','tidur','terbang']
@@ -21,4 +37,4 @@ let arrOfWord2 = ['anggi','angga','ani','adi']
 console.log(dictSort(arrOfWord1));
 console.log(dictSort(arrOfWord2));
 
-module.exports = Dictionary
+// module.exports = Dictionary
