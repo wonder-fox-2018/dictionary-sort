10c0901afed1184d4fbea6771875f7121d54a1e2
reshalem <aliisetia@gmail.com>
2018-09-04 21:20:40 +0700

    Tugas Dictionary Sort

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,32 @@
 const readline = require('readline');
-// your code here to initialize the program and take user input
 
-module.exports = Dictionary
+function selectionSort(data) {
+	for (var i = 0; i < data.length-1; i++) {
+		var temp = data[i];
+		var min = data[i];
+		var index_min;
+
+		for (var j = i+1; j < data.length; j++) {
+			if (data[j] < min) {
+				min = data[j];
+				index_min = j;
+			}
+		}
+
+		if (min < temp) {
+			data[i] = min;
+			data[index_min] = temp;
+		}
+	}
+
+	return data;
+}
+
+function dictionarySort(array_data) {
+	var array_sorted = selectionSort(array_data).join(', ');
+
+	return array_sorted;
+}
+
+console.log(dictionarySort(['makan', 'duduk', 'tidur', 'terbang'])); // duduk, makan, terbang, tidur
+console.log(dictionarySort(['anggi', 'angga', 'ani', 'adi'])); // adi, angga, anggi, ani
