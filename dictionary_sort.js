332305eee36d6ee0a75f7b70bb2762a442b9c814
Abdul Mu'min <abdmukmin@rocketmail.com>
2018-09-04 21:30:39 +0700

    push dictionary sort

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,25 @@
 const readline = require('readline');
-// your code here to initialize the program and take user input
+function dictionarySort(arrOfWord){
+    
+    for (var i=0; i<arrOfWord.length-1; i++){
+        var dataSekarang = arrOfWord[i]
+        var nilaiMin = arrOfWord[i+1]
+        var indexNilaiMin = i+1
+        for (var j=i+1; j<arrOfWord.length; j++){
+            if (arrOfWord[j]<nilaiMin){
+                nilaiMin=arrOfWord[j]
+                indexNilaiMin=j
+            }
+        }
+        //swap data awal kalo lebih isi pada comparison
+        if (nilaiMin<dataSekarang){
+            arrOfWord[i]=nilaiMin
+            arrOfWord[indexNilaiMin]=dataSekarang
+        }
+    }
+    return arrOfWord.toString()
+}
+console.log(dictionarySort(['makan','duduk','tidur','terbang']))
+console.log(dictionarySort(['anggi','angga','ani','adi']))
 
 module.exports = Dictionary
